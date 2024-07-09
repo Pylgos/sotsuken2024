d8611fbfc4639446476f76c8e9ec8cddfe79cff7
Pylgos <43234674+Pylgos@users.noreply.github.com>
2024-07-09 16:35:17 +0900

    chore: Update debug mesh materials and add option to show debug mesh

--- godot_vrrop_server/rust/src/point_cloud_visualizer.rs
+++ godot_vrrop_server/rust/src/point_cloud_visualizer.rs
@@ -1,4 +1,4 @@
-use fxhash::FxHashMap;
+use fxhash::{FxHashMap, FxHashSet};
 use godot::classes::{ImmediateMesh, MeshInstance3D, Node3D};
 use godot::engine::mesh::PrimitiveType;
 use godot::engine::Material;
@@ -10,7 +10,14 @@ use crate::binding::ImagesMessage;
 #[derive(GodotClass)]
 #[class(base=Node3D)]
 struct PointCloudVisualizer {
+    #[export]
+    debug_mesh_material_normal: Option<Gd<Material>>,
+    #[export]
+    debug_mesh_material_modified: Option<Gd<Material>>,
+    #[export]
+    show_debug_mesh: bool,
     meshes: FxHashMap<GridIndex, Gd<MeshInstance3D>>,
+    debug_mesh_inst: Gd<MeshInstance3D>,
     cloud: PointCloud,
     material: Option<Gd<Material>>,
     base: Base<Node3D>,
@@ -45,6 +52,65 @@ fn create_mesh(
     Some(mesh)
 }
 
+fn create_debug_mesh(
+    cloud: &PointCloud,
+    modified_grids: &FxHashSet<GridIndex>,
+    normal: Gd<Material>,
+    modified: Gd<Material>,
+) -> Option<Gd<ImmediateMesh>> {
+    let mut mesh = ImmediateMesh::new_gd();
+    let add_cube = |mesh: &mut Gd<ImmediateMesh>, grid_index: GridIndex| {
+        let c: [Vector3; 8] = cloud
+            .grid_map()
+            .grid_corners(grid_index)
+            .map(|v| Vector3::new(v.x, v.y, v.z));
+        mesh.surface_add_vertex(c[0]);
+        mesh.surface_add_vertex(c[1]);
+        mesh.surface_add_vertex(c[1]);
+        mesh.surface_add_vertex(c[3]);
+        mesh.surface_add_vertex(c[3]);
+        mesh.surface_add_vertex(c[2]);
+        mesh.surface_add_vertex(c[2]);
+        mesh.surface_add_vertex(c[0]);
+
+        mesh.surface_add_vertex(c[4]);
+        mesh.surface_add_vertex(c[5]);
+        mesh.surface_add_vertex(c[5]);
+        mesh.surface_add_vertex(c[7]);
+        mesh.surface_add_vertex(c[7]);
+        mesh.surface_add_vertex(c[6]);
+        mesh.surface_add_vertex(c[6]);
+        mesh.surface_add_vertex(c[4]);
+
+        mesh.surface_add_vertex(c[0]);
+        mesh.surface_add_vertex(c[4]);
+        mesh.surface_add_vertex(c[1]);
+        mesh.surface_add_vertex(c[5]);
+        mesh.surface_add_vertex(c[2]);
+        mesh.surface_add_vertex(c[6]);
+        mesh.surface_add_vertex(c[3]);
+        mesh.surface_add_vertex(c[7]);
+    };
+    mesh.call("surface_begin".into(), &[PrimitiveType::LINES.to_variant()]);
+    for grid_index in cloud
+        .grid_map()
+        .grids()
+        .keys()
+        .filter(|g| !modified_grids.contains(g))
+    {
+        add_cube(&mut mesh, *grid_index);
+    }
+    mesh.surface_end();
+    mesh.call("surface_begin".into(), &[PrimitiveType::LINES.to_variant()]);
+    for grid_index in modified_grids {
+        add_cube(&mut mesh, *grid_index);
+    }
+    mesh.surface_end();
+    mesh.surface_set_material(0, normal);
+    mesh.surface_set_material(1, modified);
+    Some(mesh)
+}
+
 #[godot_api]
 impl PointCloudVisualizer {
     #[func]
@@ -57,7 +123,7 @@ impl PointCloudVisualizer {
             return;
         };
         let modified_grids = self.cloud.merge_images_msg(image);
-        for grid_index in modified_grids {
+        for grid_index in modified_grids.iter().copied() {
             if let Some(mesh) = create_mesh(grid_index, &self.cloud, material.clone()) {
                 if let Some(mesh_inst) = self.meshes.get_mut(&grid_index) {
                     mesh_inst.set_deferred("mesh".into(), mesh.to_variant());
@@ -67,6 +133,20 @@ impl PointCloudVisualizer {
                     self.base_mut()
                         .call_deferred("add_child".into(), &[mesh_inst.to_variant()]);
                     self.meshes.insert(grid_index, mesh_inst.clone());
+                }
+            }
+        }
+
+        if self.show_debug_mesh {
+            if let (Some(normal), Some(modified)) = (
+                self.debug_mesh_material_normal.clone(),
+                self.debug_mesh_material_modified.clone(),
+            ) {
+                if let Some(mesh) =
+                    create_debug_mesh(&self.cloud, &modified_grids, normal, modified)
+                {
+                    self.debug_mesh_inst
+                        .set_deferred("mesh".into(), mesh.to_variant());
                 }
             }
         }
@@ -83,9 +163,18 @@ impl INode3D for PointCloudVisualizer {
     fn init(base: Base<Node3D>) -> Self {
         Self {
             base,
+            debug_mesh_material_normal: None,
+            debug_mesh_material_modified: None,
+            show_debug_mesh: false,
+            debug_mesh_inst: MeshInstance3D::new_alloc(),
             cloud: vrrop_server::PointCloud::new(1.0),
             meshes: FxHashMap::default(),
             material: None,
         }
+    }
+
+    fn ready(&mut self) {
+        let mesh = self.debug_mesh_inst.clone();
+        self.base_mut().add_child(mesh.upcast());
     }
 }
